30938c4168fcd87ed52e07f39e46cdf514b88e7b
bendurand <ben.durand20@gmail.com>
2022-12-08 17:48:33 +0100

    TP2 done

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 import requests
 
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -41,3 +43,56 @@ def printLogs():
     app.logger.error('testing error log')
     app.logger.info('testing info log')
     return render_template('logs.html')
+
+
+# OAUTH part
+
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+KEY_FILE_LOCATION = 'json/data-sources-deta-5cc0c71b7dba.json' # Stored in local
+VIEW_ID = '281176908'
+
+
+def initialize_analyticsreporting():
+  credentials = ServiceAccountCredentials.from_json_keyfile_name(
+      KEY_FILE_LOCATION, SCOPES)
+  analytics = build('analyticsreporting', 'v4', credentials=credentials)
+
+  return analytics
+
+
+def get_report(analytics):
+  return analytics.reports().batchGet(
+      body={
+        'reportRequests': [
+        {
+          'viewId': VIEW_ID,
+          'dateRanges': [{'startDate': '30daysAgo', 'endDate': 'today'}],
+          'metrics': [{'expression': 'ga:pageviews'}],
+          'dimensions': []
+        }]
+      }
+  ).execute()
+
+
+def get_visitors(response):
+  visitors = 0 # in case there are no analytics available yet
+  for report in response.get('reports', []):
+    columnHeader = report.get('columnHeader', {})
+    metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+
+    for row in report.get('data', {}).get('rows', []):
+      dateRangeValues = row.get('metrics', [])
+
+      for i, values in enumerate(dateRangeValues):
+        for metricHeader, value in zip(metricHeaders, values.get('values')):
+          visitors = value
+
+  return str(visitors)
+
+
+@app.route('/oauth')
+def oauth():
+    analytics = initialize_analyticsreporting()
+    response = get_report(analytics)
+    visitors = get_visitors(response)
+    return render_template('oauth.html', visitors=str(visitors))
